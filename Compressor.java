7249e296e19c9289b6fd8be63fa3f2727df3b27a
Rohan Taori <rtaori13@gmail.com>
2015-10-31 16:55:55 -0700

    format

--- Compressor.java
+++ Compressor.java
@@ -29,25 +29,25 @@ public class Compressor {
 	public static String decompress(ArrayList<Integer> compressed) {
 		HashMap<Integer,String> dictionary = new HashMap<>();
 		for (int x = 0; x < BASE_DICT; x++)
-		    dictionary.put(x, "" + (char)x);
+			dictionary.put(x, "" + (char)x);
  
-        String y = "" + (char)(int)compressed.remove(0);
+		String y = "" + (char)(int)compressed.remove(0);
 		String end = y;
 		for (int k : compressed) {
 			String entry = dictionary.containsKey(k) ? dictionary.get(k) : y + y.charAt(0);
-		    dictionary.put(dictionary.size(), y + entry.charAt(0));
-		    y = entry;
-		    end += entry;
+			dictionary.put(dictionary.size(), y + entry.charAt(0));
+			y = entry;
+			end += entry;
 		}
-        
-        return end;
-    }
- 
-    public static void main(String[] args) {
-        ArrayList<Integer> compressed = compress("ababababab");
-        System.out.println(compressed);
-        String decompressed = decompress(compressed);
-        System.out.println(decompressed);
-        System.out.println((char)500);
-    }
+
+		return end;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> compressed = compress("ababababab");
+		System.out.println(compressed);
+		String decompressed = decompress(compressed);
+		System.out.println(decompressed);
+		System.out.println((char)500);
+	}
 }
