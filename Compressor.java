521a7edbd1a0cc82ddb26a86bb10794c37d23250
Rohan Taori <rtaori13@gmail.com>
2015-10-31 18:09:21 -0700

    added fileIO funcitonality

--- Compressor.java
+++ Compressor.java
@@ -1,3 +1,9 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
  
 public class Compressor {
@@ -32,22 +38,59 @@ public class Compressor {
 			dictionary.put(x, "" + (char)x);
  
 		String y = "" + (char)(int)compressed.remove(0);
-		String end = y;
+		StringBuilder end = new StringBuilder(y);
 		for (int k : compressed) {
 			String entry = dictionary.containsKey(k) ? dictionary.get(k) : y + y.charAt(0);
 			dictionary.put(dictionary.size(), y + entry.charAt(0));
 			y = entry;
-			end += entry;
+			end.append(entry);
 		}
 
-		return end;
+		return end.toString();
+	}
+	
+	public static String readWords(String file) throws FileNotFoundException {
+		Scanner reader = new Scanner(new File(file));
+		StringBuilder text = new StringBuilder();
+		while(reader.hasNextLine())
+			text.append(reader.nextLine());
+		reader.close();
+		return text.toString();
+	}
+	
+	public static void writeCompressed(ArrayList<Integer> compressed, String file) throws IOException {
+		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+		for(int x : compressed) {
+			writer.println(x);
+		}
+		writer.close();
+	}
+	
+	public static ArrayList<Integer> readCompressed(String file) throws FileNotFoundException {
+		Scanner reader = new Scanner(new File(file));
+		ArrayList<Integer> compressed = new ArrayList<>();
+		while(reader.hasNextInt())
+			compressed.add(reader.nextInt());
+		reader.close();
+		return compressed;
+	}
+	
+	public static void writeWords(String text, String file) throws IOException {
+		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+		writer.println(text);
+		writer.close();
 	}
 
-	public static void main(String[] args) {
-		ArrayList<Integer> compressed = compress("ababababab");
-		System.out.println(compressed);
-		String decompressed = decompress(compressed);
-		System.out.println(decompressed);
-		System.out.println((char)500);
-	}
+//	public static void main(String[] args) {
+//		try {
+//			String text = readWords("inputWords2.txt");
+//			ArrayList<Integer> compressed = compress(text);
+//			writeCompressed(compressed, "outputCompressed.txt");
+//			ArrayList<Integer> outFile = readCompressed("outputCompressed.txt");
+//			String words = decompress(outFile);
+//			writeWords(words, "outputWords.txt");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
